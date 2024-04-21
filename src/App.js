b3652effa31475addd38412e698524ad506b3a41
Aslak Aarflot Jønsson <aslakaaj@gmail.com>
2024-04-21 20:18:37 +0200

    Added page structre

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import name_logo from "./img/name_logo.svg";
-
+import "./App.css";
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <img src={name_logo} alt="Aslak Jønsson" />
+      <Header/>
+      <div className="services">
+        <h2 className="services_header">Services</h2>
+        <ul>
+          <li><h4>Building for your needs</h4><br /> <p>Building websites for your business´ needs and scale.</p> </li>
+          <li><h4>Design in modern fashion</h4><p>Before project start, you´ll join in on the design process, and shaping your future site.</p></li>
+          <li><h4>Customer Portal</h4><p>A simpler two-way comminication. Client have full control over the developing process.</p></li>
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import name_logo from "../img/name_logo.svg";
+import "./Header"
+
+var Header = () => {
+  return (
+    <div>
+      <img src={name_logo} alt="Aslak Jønsson" />
+      <p>
+        I´m a Front-End developer, focusing on both Interactive and Dynamic
+        Design. Working mainly with local businesses. I design and develop
+        websites that fits your brand. Currently I´m based in Volda, Norway.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
