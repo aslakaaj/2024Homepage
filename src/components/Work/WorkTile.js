f8e5d6e62c51ddca781d692ed57f370020ebc12e
Aslak Aarflot Jønsson <aslakaaj@gmail.com>
2024-05-02 08:59:23 +0200

    Added dynamic content and made tweeks to the design

--- src/components/Work/WorkTile.js
+++ src/components/Work/WorkTile.js
@@ -1,12 +1,67 @@
 import "./WorkTile.css";
-import img from "../../img/tandoori.webp";
+import { useCallback, useEffect } from "react";
+import { useState } from "react";
 
-const WorkTile = () => {
-    return (
-        <div className="tile_box" style={{backgroundImage: `url(${img})`}}>
-            
-        </div>
-    );
-}
+const WorkTile = (props) => {
+  const [imgSrc, setImgSrc] = useState(null);
+  const [screenWidth, setScreenWidth] = new useState(window.innerWidth);
+
+  const handleWindowSizeChange = useCallback(() => {
+    setScreenWidth(window.innerWidth);
+  }, [setScreenWidth]);
+
+  const clickHandler = () => {
+    window.open(props.link);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    import(`../../img/${props.img}`)
+      .then((image) => {
+        setImgSrc(image.default);
+      })
+      .catch((error) => {
+        console.error("Error importing image: ", error);
+      });
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, [props.img, handleWindowSizeChange]);
+
+  return (
+    <li onClick={clickHandler} className="tile_item">
+      {screenWidth <= 764 && (
+        <>
+          <div
+            className="tile_box"
+            style={{ backgroundImage: `url(${imgSrc})` }}
+          >
+            <div className="tile_box_blur">
+              <div className="tile_tag">{props.tag01}</div>
+              <div className="tile_tag">{props.tag02}</div>
+            </div>
+          </div>
+          <h2 data={props.emoji}>{props.title}</h2>
+        </>
+      )}
+      {screenWidth >= 764 && (
+        <>
+          <div
+            className="tile_box"
+            style={{ backgroundImage: `url(${imgSrc})` }}
+          >
+            <div className="tile_box_blur">
+              <h2 data={props.emoji}>{props.title}</h2>
+              <div className="tile_box_tags">
+                <div className="tile_tag">{props.tag01}</div>
+                <div className="tile_tag">{props.tag02}</div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </li>
+  );
+};
 
 export default WorkTile;
--- src/components/WorkList.js
+++ src/components/WorkList.js
@@ -1,10 +1,21 @@
 import WorkTile from "./Work/WorkTile";
+import data from "../data/data.json";
 import "./WorkList.css";
 
 const WorkList = () => {
     return (
         <ul className="work_list">
-            <WorkTile/>
+            {data.work.map((work) => (
+                <WorkTile
+                    key={Math.random()}
+                    title={work.title}
+                    emoji={work.emoji}
+                    tag01={work.tag01}
+                    tag02={work.tag02}
+                    link={work.link}
+                    img={work.img}
+                />
+            ))}
         </ul>
     );
 }
